61516954fb114e63c60f2daf79673b04250b83ce
Corvette Rox <development@thzero.com>
2025-02-10 20:58:42 -0600

    memory checker

--- constants.h
+++ constants.h
@@ -18,12 +18,15 @@
 #define SERIAL_BAUD 115200
 #define STATUS_HEIGHT_BAR 10
 
+// Sample rate is in Hz
 // Based on Eggtimer Quantum documentation
 // https://eggtimerrocketry.com/wp-content/uploads/2024/02/Eggtimer-Quantum-1_09G.pdf, page 25
 #define SAMPLE_RATE_GROUND 10
 #define SAMPLE_RATE_AIRBORNE_ASCENT 20
 #define SAMPLE_RATE_AIRBORNE_DESCENT 2
 #define SAMPLE_MEASURES_APOGEE 5
+
+#define SAMPLE_RATE_MEMORY 1
 
 // #define USE_SLEEP_MODE
 
